06a8a57d220574845461bf2d8b1c23eccd62185d
Humaon <ehumaonkabir39@gmail.com>
2022-06-25 15:57:12 +0600

    unique email

--- backend/remoteOffice.sql
+++ backend/remoteOffice.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 24, 2022 at 01:44 PM
+-- Generation Time: Jun 25, 2022 at 09:55 AM
 -- Server version: 10.6.7-MariaDB-2ubuntu1
 -- PHP Version: 8.1.2
 
@@ -44,7 +44,10 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-
+INSERT INTO `users` (`id`, `name`, `profileImage`, `dateOfBirth`, `email`, `attachment`, `password`, `createdAt`, `updatedAt`, `role`) VALUES
+(1, 'emon 6', 'uploads/profileImage-1655983663087.jpg', '2020-05-02', 'ehumaonkabir39@gmail.com', 'uploads/attachment-1655983663088.pdf', '$2a$12$pAQtjWnB6EVzeDZmfeiMvOAqtn7kVOW3izeRE5Y4I8LNbtm5x.bUK', '2022-06-23 17:27:43', '2022-06-23 17:27:43', 'admin'),
+(2, 'emon 9', 'uploads/profileImage-1655983663087.jpg', '2020-05-02', 'ehumaonkabir39@gmail.comjk', 'uploads/attachment-1655983663088.pdf', '$2a$12$pAQtjWnB6EVzeDZmfeiMvOAqtn7kVOW3izeRE5Y4I8LNbtm5x.bUK', '2022-06-23 17:27:43', '2022-06-23 17:27:43', 'user'),
+(3, 'Humaon', 'uploads/profileImage-1656147990152.png', 'Wed Jun 15 2022 00:00:00 GMT+0600 (Bangladesh Standard Time)', 'abcd@gmail.com', 'uploads/attachment-1656147990159.jpg', '$2a$12$LcNNO.3220yqkxppRP8Us.jlcFZaCUxItMH9NBLu7QbQYi/waaRwm', '2022-06-25 15:06:30', '2022-06-25 15:06:30', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -54,7 +57,8 @@ CREATE TABLE `users` (
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -64,7 +68,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
